f9e2a437d65a314de858ad3d0a51cf7da882d2ba
Florian Sauer <flomanuel.github@gmail.com>
2023-12-12 11:17:05 +0100

    remove unused import

--- src/main/java/com/acme/verlag/entity/Preis.java
+++ src/main/java/com/acme/verlag/entity/Preis.java
@@ -25,7 +25,6 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
